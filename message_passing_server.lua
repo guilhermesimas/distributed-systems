226ba4734173d28e9b8f2233269c37ac1420dfb7
Guilherme Simas <guilherme_abinader@hotmail.com>
2017-04-04 20:35:25 -0300

    First implementaion of client server message passing

---
+++ message_passing_server.lua
@@ -0,0 +1,23 @@
+-- This program aims to establish a connection between a client and a server 
+-- that will be responsible for passing 1K strings. After passing the message 
+-- the connection will be shut down
+-- Authors: github.com/guilhermesimas ; github.com/claraszw
+-- Date: 04.apr.2017
+
+-- loading namespace
+local socket = require("socket")
+
+-- gettin TCP connection, with local host IP address and to any port (0)
+local server = assert(socket.bind("*",0))
+
+local ip, port = server:getsockname()
+
+print("Local IP:" .. ip .. " Port:" .. port)
+
+local client = server:accept()
+
+print "Client connected"
+
+client:send("Hello client")
+
+client:shutdown("both")
