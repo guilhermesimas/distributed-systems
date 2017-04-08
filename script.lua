6768a45d2b26a1dec8f944d801836f3ac496e4ff
Guilherme Simas <guilherme_abinader@hotmail.com>
2017-04-07 22:28:50 -0300

    Opens a server connection for each client in order to make sure port is available

--- script.lua
+++ script.lua
@@ -4,26 +4,27 @@
 print("============INTERRUPTED SCRIPT===============")
 
 -- starts server
-local server = assert(io.popen("lua5.3 server.lua","r"))
 -- sets timeout for the server so the process does not get stuck
 -- in case of failure from counter-part
 -- gets server output, which is the port number
-local port = server:read("n")
 local n=1;
 while n<=10000000 do
 	print("---------BEGIN N="..n.."--------------")
+	local server = assert(io.popen("lua5.3 server.lua","r"))
+	assert(os.execute("sleep 1"))
+	local port = server:read("n")
 	-- executes client passing in params
 	assert(os.execute("lua5.3 client.lua "..port.." "..n))
+	assert(os.execute("sleep 1"))
+	server:close()
 	print("---------END N="..n.."--------------")
 	n=n*10
 end
 
 local socket = require("socket")
 local server_connection = assert(socket.connect("*",port))
-server_connection:settimeout(5)
 assert(server_connection:setoption('tcp-nodelay',true))
 server_connection:send("quit\n")
-server:shutdown()
 server_connection:close()
 
 print("============END SCRIPT===============")
