acf0eba902e12e52d1b86474710c844a843f8c9b
Guilherme Simas <guilherme_abinader@hotmail.com>
2017-04-08 11:23:50 -0300

    Still unstable

--- script.lua
+++ script.lua
@@ -6,12 +6,10 @@ print("============INTERRUPTED SCRIPT===============")
 
 -- starts server
 local server = assert(io.popen("lua5.3 server.lua","r"))
--- sets timeout for the server so the process does not get stuck
--- in case of failure from counter-part
 -- gets server output, which is the port number
 local port = server:read("n")
 local n=1;
-while n<=10000000 do
+while n<=100000000 do
 	print("---------BEGIN N="..n.."--------------")
 	-- executes client passing in params
 	assert(os.execute("lua5.3 client.lua "..port.." "..n))
@@ -19,7 +17,7 @@ while n<=10000000 do
 	n=n*10
 end
 
-local server_connection = assert(socket.connect("*",port))
+local server_connection = assert(socket.connect("0.0.0.0",port))
 assert(server_connection:setoption('tcp-nodelay',true))
 server_connection:send("quit\n")
 server_connection:close()
--- server.lua
+++ server.lua
@@ -9,18 +9,20 @@ local socket = require("socket")
 
 -- gettin TCP connection, with local host IP address and to any port (0)
 local server = assert(socket.bind("*",0))
-assert(server:setoption('tcp-nodelay',true))
-assert(server:setoption('reuseaddr',true))
-server:settimeout(5)
 local ip, port = server:getsockname()
-
 print(port)
+-- assert(server:setoption('tcp-nodelay',true))
+server:settimeout(20)
 
 local file = assert(io.open("./lyrics.txt","r"))
 local string = file:read(1024);
 local client
 while 1 do
-	client = server:accept()
+	client = assert(server:accept())
+	if client==nil then
+		server:close()
+		return
+	end
 	local message = client:receive("*l")
 	if message == "quit" then
 		break
@@ -28,4 +30,5 @@ while 1 do
 	client:send(string)
 	client:close()
 end
+server:close()
 client:close()
