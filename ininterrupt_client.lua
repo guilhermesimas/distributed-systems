1e12e50ce76c1077c0219a4511cd1542cdf8c882
claraszw <clara_szw@hotmail.com>
2017-04-08 09:54:05 -0300

    Server writes port number in file read by client. Number of requests is an argument in client. Client prints time on screen.

--- ininterrupt_client.lua
+++ ininterrupt_client.lua
@@ -1,26 +1,29 @@
 -- This program aims to establish a connection between a client and a server 
--- that will be responsible for passing 1K strings. After passing the message 
--- the connection will be shut down
+-- that will be responsible for passing 1K strings. The client can make several
+-- requests with the same connection. The connection will be shutdown
+-- only when the client disconnects.
 -- Authors: github.com/guilhermesimas ; github.com/claraszw
 -- Date: 04.apr.2017
 
 -- loading namespace
 local socket = require("socket")
 
--- gettin TCP connection, with local host IP address and to any port (0)
-local server = assert(socket.connect("0.0.0.0",arg[1]))
+-- get server port number
+local file_port = io.open("port.txt","r")
+local port = file_port:read("*n")
+
+-- gettin TCP connection, with local host IP address and server port
+local server = assert(socket.connect("0.0.0.0",port))
 local t_init = socket.gettime()
 
-for i=1,arg[2] do
-	--local request = io.read("l").."\n"
+for i=1,arg[1] do
 	server:send("download\n")
-	-- if request =="quit\n" then
-	-- 	break;
-	-- end
 	local message = server:receive(1024)
 end
 
 local t_end = socket.gettime()
 
-print("Total client ininterrupted time was " .. t_end-t_init .. "seconds \n")
+server:send("quit\n")
 server:close()
+print("Total client ininterrupted time was " .. t_end-t_init .. "seconds \n")
+
--- ininterrupt_server.lua
+++ ininterrupt_server.lua
@@ -1,6 +1,7 @@
 -- This program aims to establish a connection between a client and a server 
--- that will be responsible for passing 1K strings. After passing the message 
--- the connection will be shut down
+-- that will be responsible for passing 1K strings. The client can make several
+-- requests with the same connection. The connection will be shutdown
+-- only when the client disconnects.
 -- Authors: github.com/guilhermesimas ; github.com/claraszw
 -- Date: 04.apr.2017
 
@@ -9,19 +10,20 @@ local socket = require("socket")
 
 -- gettin TCP connection, with local host IP address and to any port (0)
 local server = assert(socket.bind("*",0))
+assert(server:setoption('tcp-nodelay',true))
 
+-- get port number and write it in a file 
 local ip, port = server:getsockname()
-
-print(port)
+local file_port = assert(io.open("port.txt","w"))
+file_port:write(port)
+file_port:close()
 
 local file = assert(io.open("lyrics.txt","r"))
 local string = file:read(1024)
 file:close()
 
+-- accept client conection
 local client = server:accept()
-local t_init = socket.gettime()
-print "Client connected"
-
 
 while 1 do
 	local message = client:receive("*l")
@@ -31,7 +33,6 @@ while 1 do
 	client:send(string)
 end
 
-local t_end = socket.gettime()
-
-print("Total server ininterrupted time was " .. t_end-t_init .. "seconds \n")
+-- close client connection and delete port file
 client:close()
+os.execute("rm port.txt")
