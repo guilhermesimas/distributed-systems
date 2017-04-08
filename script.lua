5d94fde998c108e9536408b7cb9814af4c7d39fa
Guilherme Simas <guilherme_abinader@hotmail.com>
2017-04-08 12:41:46 -0300

    Stable. Script included for both models

--- script.lua
+++ script.lua
@@ -3,30 +3,31 @@
 
 print("============INTERRUPTED SCRIPT===============")
 
--- starts server
--- sets timeout for the server so the process does not get stuck
--- in case of failure from counter-part
--- gets server output, which is the port number
+os.execute("rm port1.txt")
 local n=1;
-local port
-while n<=10000000 do
+while n<=100000 do
 	print("---------BEGIN N="..n.."--------------")
-	assert(os.execute("lua5.3 server.lua>port.txt &"))
+	assert(os.execute("lua5.3 server.lua &"))
 	assert(os.execute("sleep 1"))
-	local port_file = io.open("port.txt","r")
-	port = port_file:read("n")
-	print("Using port "..port)
-	assert(os.execute("rm port.txt"))
+	assert(os.execute("lua5.3 client.lua "..n))
 	-- executes client passing in params
-	assert(os.execute("lua5.3 client.lua "..port.." "..n))
-	assert(os.execute("sleep 1"))
 	print("---------END N="..n.."--------------")
 	n=n*10
 end
 
-local socket = require("socket")
-local server_connection = assert(socket.connect("0.0.0.0",port))
-server_connection:send("quit\n")
-server_connection:close()
+print("============END SCRIPT===============")
+print("============ININTERRUPTED SCRIPT===============")
+
+os.execute("rm port.txt")
+local n=1;
+while n<=100000 do
+	print("---------BEGIN N="..n.."--------------")
+	assert(os.execute("lua5.3 ininterrupt_server.lua &"))
+	assert(os.execute("sleep 1"))
+	assert(os.execute("lua5.3 ininterrupt_client.lua "..n))
+	-- executes client passing in params
+	print("---------END N="..n.."--------------")
+	n=n*10
+end
 
 print("============END SCRIPT===============")
--- server.lua
+++ server.lua
@@ -10,7 +10,7 @@ local socket = require("socket")
 -- gettin TCP connection, with local host IP address and to any port (0)
 local server = assert(socket.bind("*",0))
 assert(server:setoption('tcp-nodelay',true))
---server:settimeout(5)
+server:settimeout(5)
 local ip, port = server:getsockname()
 
 local file_port = io.open("port1.txt","w")
