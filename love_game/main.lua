cf50d04676149e881cbc6c0ca51d9486df7bcc87
Guilherme Simas <guilherme_abinader@hotmail.com>
2017-06-29 18:03:35 -0300

    moving square

--- love_game/main.lua
+++ love_game/main.lua
@@ -1,3 +1,27 @@
+speed = 100;
+
+function love.keypressed(key)
+    direction = key
+end
+
+function love.load()
+    direction = "right";
+    quadrado = {x = 20,y = 20}
+end
+
+function love.update(dt)
+    if direction == "right" then
+        quadrado.x = quadrado.x + dt*speed;
+    elseif direction == "left" then
+        quadrado.x = quadrado.x - dt*speed;
+    elseif direction == "down" then
+        quadrado.y = quadrado.y + dt*speed;
+    elseif direction == "up" then
+        quadrado.y = quadrado.y - dt*speed;
+    end
+
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.rectangle("fill",quadrado.x,quadrado.y,20,20)
 end
